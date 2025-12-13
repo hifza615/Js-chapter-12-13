d1b52ecfb0817a8a72a5eebe6e1d97d8372622b3
HIFZA EMAAN <hifza.4.106023@gmail.com>
2025-12-14 03:03:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,134 @@
+//****CHAPTER 12-13********//
+
+//****CHAPTER 12-13****QUESTION 2****//
+//var num1 = Number(prompt("Enter first integer:"));
+//var num2 = Number(prompt("Enter second integer:"));
+
+//if (num1 > num2) {
+    //document.write("The larger number is: " + num1);}
+//else if (num2 > num1) {
+   // document.write("The larger number is: " + num2);}
+//else {
+   // document.write("Both numbers are equal.");}
+   
+   
+//****CHAPTER 12-13****QUESTION 3****//
+
+//var num = Number(prompt("Enter a number:"));
+
+//if (num > 0) {
+    //document.write("The number is Positive.");}
+//else if (num < 0) {
+    //document.write("The number is Negative.");}
+//else {
+    //document.write("The number is Zero.");}
+
+//****CHAPTER 12-13****QUESTION 4****//
+
+//var character = prompt("Enter a alphabet:");
+
+//character = character;
+
+//if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u") {
+  //  document.write("true");}
+//else {
+   // document.write("false");}
+
+//****CHAPTER 12-13****QUESTION 5****//
+
+//var correctPassword = "Hifza";
+//var userPassword = prompt("Enter your password:");
+
+//if (userPassword === "" || userPassword === null) {
+    //document.write("Please enter your password");} 
+//else if (userPassword === correctPassword) {
+   // document.write("Correct! The password you entered matches the original password");} 
+//else {
+    //document.write("Incorrect password");}
+
+//****CHAPTER 12-13****QUESTION 6****//
+
+//var greeting;
+//var hour = 13;
+
+//if (hour < 18) {
+  //  greeting = "Good day";}
+   // else {
+    //greeting = "Good evening";}
+//document.write(greeting);
+
+//****CHAPTER 12-13****QUESTION 7****//
+
+//var time = prompt("Enter a Time in 24 hour format");
+
+//if(time === "00"){
+//  alert( "Time : " + time + " = 12am") }
+
+// else if(time === "01"){
+  //   alert("Time : "+time+" =1am") }
+
+// else if(time === "02"){
+  //   alert("Time : "+time+" =2am") }
+// else if(time === "03"){
+   //  alert("Time : "+time+" = 3am") }
+
+//else if(time === "04"){
+   //alert("Time : "+time+" = 4am") }
+
+//else if(time === "05"){
+  // alert("Time : "+time+" = 5am")}
+
+// else if(time === "06"){
+   // alert("Time : "+time+" = 6am")}
+
+//else if(time === "07")  
+//{alert("Time : "+time+" = 7am")}
+
+// else if(time === "08"){
+  // alert("Time : "+time+" = 8am") }
+//else if(time === "09"){
+//   alert("Time : "+time+" = 9am") }
+
+//else if(time === "10"){
+   //  alert("Time : "+time+" = 10am") }
+
+// else if(time === "11"){
+   // alert("Time : "+time+" = 11am")}
+
+//else if(time === "12"){
+  //  alert("Time : "+time+" = 12pm") }
+
+// else if(time === "13"){
+  //  alert("Time : "+time+" = 1pm") }
+
+//else if(time === "14"){
+  // alert("Time : "+time+" = 2pm") }
+
+// else if(time === "15"){
+  //   alert("Time : "+time+" = 3pm") }
+
+// else if(time === "16")  {
+ //alert("Time : "+time+" = 4pm") }
+
+// else if(time === "17"){
+  // alert("Time : "+time+" = 5pm") }
+
+//else if(time === "18"){
+  //   alert("Time : "+time+" = 6pm") }
+
+//else if(time === "19"){
+  //alert("Time : "+time+" = 7pm") }
+
+ //else if(time === "20"){
+   //  alert("Time : "+time+" = 8pm") }
+
+//else if(time === "21"){
+  //   alert("Time : "+time+" = 9pm") }
+
+// else if(time === "22"){
+ //  alert("Time : "+time+" = 10pm") }
+
+// else if(time === "23"){
+  // alert("Time : "+time+ " = 11pm") }
+// else{
+ // alert("Wrong Enter ! Time format is 00, 01, 02, 04 ...13,14,15...") }
